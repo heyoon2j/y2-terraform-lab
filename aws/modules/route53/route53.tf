f14cd9bcd97d29315dc711b33e67c613e9dd2eae
heyoon2j <jyszz1201@gmail.com>
2021-12-08 18:14:49 +0900

    route53 test

--- aws/modules/route53/route53.tf
+++ aws/modules/route53/route53.tf
@@ -2,13 +2,11 @@
 
 /*
 1. Hostzone
-    1) Zone 생성
-    2) 
-    3) 
-    4) Routing Table의 Route 추가
-    5) Routing Table Association
-2. Record
-    1) EIP 생성
+    1) Hosting Zone 생성
+    2) Domain 생성 및 Name Server 등록 (AWS 뿐만 아니라 다른 DNS 대행 업체 이용 가능)
+    3) Record 생성
+2. VPC
+    1) DHCP 옵션 세트 생성
     2) NAT Gateway 생성
     2) Routing Table의 Route 추가
 3. IGW Gateway
